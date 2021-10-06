377114665886563537607b04d4197a1c478bd418
MichonneHsu <850574565@qq.com>
2021-10-06 15:24:25 +0800

    dd

--- src/Config.php
+++ src/Config.php
@@ -7,7 +7,9 @@ class Config{
         'domain' => 'https://oapi.dingtalk.com',
         'gettoken' => '/gettoken',
         'getuserinfo'=>'/user/getuserinfo',
-        'createuser'=>'/topapi/v2/user/create'
+        'createuser'=>'/topapi/v2/user/create',
+        'updateuser'=>'/topapi/v2/user/update',
+        'deleteuser'=>'/topapi/v2/user/delete'
     ];
     public static $app_info=[
       
--- src/User.php
+++ src/User.php
@@ -19,4 +19,20 @@ class User{
        
         return apiRequest::post($uri, $json);
     }
+    public static function update_user(array $json){
+        $uri=apiRequest::joinParams(Config::$api['updateuser'],[
+            'access_token'=>AccessToken::getToken()
+        ]);
+        
+        return apiRequest::post($uri, $json);
+    }
+    public static function remove_user(string $userid){
+        $uri=apiRequest::joinParams(Config::$api['deleteuser'],[
+            'access_token'=>AccessToken::getToken()
+        ]);
+        $json=[
+            'userid'=>$userid
+        ];
+        return apiRequest::post($uri, $json);
+    }
 } 
