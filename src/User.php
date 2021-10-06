b10ed926b6ee98b0b48e375ef886eeef76eccef1
MichonneHsu <850574565@qq.com>
2021-10-06 11:20:41 +0800

    dd

--- src/User.php
+++ src/User.php
@@ -1,16 +1,14 @@
 <?php declare(strict_types=1);
 namespace SimpleDingtalk;
 
-use SimpleDingTalk\apiRequest;
-
 final class User{
     public static function getuserinfo(string $code){
-        $uri = apiRequest::joinParams(Uri::$api['getuserinfo'],[
-            'access_token'=>
+        $uri = apiRequest::joinParams(Config::$api['getuserinfo'],[
+            'access_token'=>AccessToken::getToken()
         ]);
         $query = [
             'code' => $code
         ];
-        $json = apiRequest::get($uri, $query);
+        return apiRequest::get($uri, $query);
     }
 } 
