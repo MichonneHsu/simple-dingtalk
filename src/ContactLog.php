d446b7ecd8f7f7862fe14fcf45aba3be6bc6d54d
MichonneHsu <850574565@qq.com>
2021-10-08 17:40:15 +0800

    dd

--- src/ContactLog.php
+++ src/ContactLog.php
@@ -1,42 +1,80 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
 namespace SimpleDingTalk;
 
-class ContactLog{
-    public static function get_department(int $dept_id){
-        $uri=apiRequest::joinParams(Config::$api['contact_log']['get_department'],[
-            'access_token'=>AccessToken::getToken()
+class ContactLog
+{
+    /**
+     * 获取部门详情
+     *
+     * @param integer $dept_id
+     * @return mixed
+     */
+    public static function get_department(int $dept_id)
+    {
+        $uri = apiRequest::joinParams(Config::$api['contact_log']['get_department'], [
+            'access_token' => AccessToken::getToken()
         ]);
-        $json=[
-            'dept_id'=>$dept_id
+        $json = [
+            'dept_id' => $dept_id
         ];
         return apiRequest::post($uri, $json);
     }
-    public static function get_user(array $json){
-        $uri=apiRequest::joinParams(Config::$api['contact_log']['get_user'],[
-            'access_token'=>AccessToken::getToken()
+    /**
+     * 获取部门用户详情
+     *
+     * @param integer $json
+     * @return mixed
+     */
+    public static function get_user(array $json)
+    {
+        $uri = apiRequest::joinParams(Config::$api['contact_log']['get_user'], [
+            'access_token' => AccessToken::getToken()
         ]);
-       
+
         return apiRequest::post($uri, $json);
     }
-    public static function get_user_list(array $json){
-        $uri=apiRequest::joinParams(Config::$api['contact_log']['get_user_list'],[
-            'access_token'=>AccessToken::getToken()
+    /**
+     * 获取部门下人员列表
+     *
+     * @param integer $json
+     * @return mixed
+     */
+    public static function get_user_list(array $json)
+    {
+        $uri = apiRequest::joinParams(Config::$api['contact_log']['get_user_list'], [
+            'access_token' => AccessToken::getToken()
         ]);
-       
+
         return apiRequest::post($uri, $json);
     }
-    public static function get_department_list(array $json){
-        $uri=apiRequest::joinParams(Config::$api['contact_log']['get_department_list'],[
-            'access_token'=>AccessToken::getToken()
+    /**
+     * 获取部门列表
+     *
+     * @param integer $json
+     * @return mixed
+     */
+    public static function get_department_list(array $json)
+    {
+        $uri = apiRequest::joinParams(Config::$api['contact_log']['get_department_list'], [
+            'access_token' => AccessToken::getToken()
         ]);
-       
+
         return apiRequest::post($uri, $json);
     }
-    public static function get_organization(){
-        $uri=apiRequest::joinParams(Config::$api['contact_log']['get_organization'],[
-            'access_token'=>AccessToken::getToken()
+    /**
+     * 获取企业信息
+     * 
+     * @return mixed
+     */
+    public static function get_organization()
+    {
+        $uri = apiRequest::joinParams(Config::$api['contact_log']['get_organization'], [
+            'access_token' => AccessToken::getToken()
         ]);
-       
+
         return apiRequest::post($uri);
     }
 }
