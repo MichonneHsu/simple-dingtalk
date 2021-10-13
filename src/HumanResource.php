c63165af664c8dba5e1e0d748fac9ca15ce7bd60
MichonneHsu <850574565@qq.com>
2021-10-13 16:44:44 +0800

    update

--- src/HumanResource.php
+++ src/HumanResource.php
@@ -2,7 +2,7 @@
 namespace SimpleDingTalk;
 
 class HumanResource{
-    public static function queryonjob(string $status_list,int $offset=0,int $size)
+    public static function queryonjob(string $status_list,int $size,int $offset=0)
     {
         $uri = Url::joinParams(Url::$api['humanResource']['queryonjob'], [
             'access_token' => AccessToken::getToken()
@@ -14,28 +14,37 @@ class HumanResource{
         ];
         return apiRequest::post($uri, $json);
     }
-    public static function querypreentry(array $json)
+    public static function querypreentry(int $size,int $offset=0)
     {
         $uri = Url::joinParams(Url::$api['humanResource']['querypreentry'], [
             'access_token' => AccessToken::getToken()
         ]);
-       
+        $json=[
+            'offset'=>$offset,
+            'size'=>$size
+        ];
         return apiRequest::post($uri, $json);
     }
-    public static function listdimission(array $json)
+    public static function listdimission(string $userid_list)
     {
         $uri = Url::joinParams(Url::$api['humanResource']['listdimission'], [
             'access_token' => AccessToken::getToken()
         ]);
-       
+        $json=[
+            'userid_list'=>$userid_list
+        ];
         return apiRequest::post($uri, $json);
     }
-    public static function querydimission(array $json)
+    public static function querydimission(int $size,int $offset=0)
     {
         $uri = Url::joinParams(Url::$api['humanResource']['querydimission'], [
             'access_token' => AccessToken::getToken()
         ]);
-       
+        $json=[
+            'offset'=>$offset,
+            'size'=>$size
+        ];
+     
         return apiRequest::post($uri, $json);
     }
     public static function addpreentry(array $json)
@@ -43,7 +52,7 @@ class HumanResource{
         $uri = Url::joinParams(Url::$api['humanResource']['addpreentry'], [
             'access_token' => AccessToken::getToken()
         ]);
-       
+        
         return apiRequest::post($uri, $json);
     }
 }
