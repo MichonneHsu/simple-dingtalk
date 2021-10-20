38ab406aeab5575cd07f1954eb4c609f1d090536
MichonneHsu <850574565@qq.com>
2021-10-20 16:00:40 +0800

    dd

--- src/v2/Todo.php
+++ src/v2/Todo.php
@@ -8,75 +8,85 @@ namespace SimpleDingTalk\v2;
 
 class Todo
 {
-    public static function create(array $body){
-        $unionId=UserInfo::$unionId;
-        $uri=Url::$api['todo']."{$unionId}/tasks";
-        $params=[
-            'operatorId'=>$unionId
+    public static function create(array $body)
+    {
+        $unionId = UserInfo::$unionId;
+        $uri = Url::$api['todo'] . "{$unionId}/tasks";
+        $params = [
+            'operatorId' => $unionId
         ];
-        $uri=apiRequest::joinParams($uri,$params);
-       
-        return apiRequest::post($uri,$body);
+        $uri = apiRequest::joinParams($uri, $params);
+
+        return apiRequest::post($uri, $body);
     }
-  
-    public static function get(string $id){
-        $unionId=UserInfo::$unionId;
-        $uri=Url::$api['todo']."{$unionId}/tasks/$id";
-       
+
+    public static function get(string $id)
+    {
+        $unionId = UserInfo::$unionId;
+        $uri = Url::$api['todo'] . "{$unionId}/tasks/$id";
+
         return apiRequest::get($uri);
     }
-    public static function remove(string $id){
-        $unionId=UserInfo::$unionId;
-        $uri=Url::$api['todo']."{$unionId}/tasks/$id";
-        $params=[
-            'operatorId'=>$unionId
+    public static function remove(string $id)
+    {
+        $unionId = UserInfo::$unionId;
+        $uri = Url::$api['todo'] . "{$unionId}/tasks/$id";
+        $params = [
+            'operatorId' => $unionId
         ];
-        $uri=apiRequest::joinParams($uri,$params);
+        $uri = apiRequest::joinParams($uri, $params);
 
         return apiRequest::delete($uri);
     }
-    public static function update(string $id,array $body){
-        $unionId=UserInfo::$unionId;
-        $uri=Url::$api['todo']."{$unionId}/tasks/$id";
-        $params=[
-            'operatorId'=>$unionId
+    public static function update(string $id, array $body)
+    {
+        $unionId = UserInfo::$unionId;
+        $uri = Url::$api['todo'] . "{$unionId}/tasks/$id";
+        $params = [
+            'operatorId' => $unionId
         ];
-        $uri=apiRequest::joinParams($uri,$params);
-       
-        return apiRequest::put($uri,$body);
+        $uri = apiRequest::joinParams($uri, $params);
+
+        return apiRequest::put($uri, $body);
     }
-    public static function status(string $id,bool $isDone){
-        $unionId=UserInfo::$unionId;
-        $uri=Url::$api['todo']."{$unionId}/tasks/$id/executorStatus";
-        $params=[
-            'operatorId'=>$unionId
+    public static function status(string $id, bool $isDone)
+    {
+        $unionId = UserInfo::$unionId;
+        $uri = Url::$api['todo'] . "{$unionId}/tasks/$id/executorStatus";
+        $params = [
+            'operatorId' => $unionId
         ];
-        $uri=apiRequest::joinParams($uri,$params);
-        $body=[
-            'executorStatusList'=>[
-                'id'=>$unionId,
-                'isDone'=>$isDone
+        $uri = apiRequest::joinParams($uri, $params);
+        $body = [
+            'executorStatusList' => [
+                [
+                    'id' => $unionId,
+                    'isDone' => $isDone
+                ]
+
             ]
         ];
-        return apiRequest::put($uri,$body);
+        return apiRequest::put($uri, $body);
     }
-    public static function get_by_sourceId(string $sourceId){
-        $unionId=UserInfo::$unionId;
-        $uri=Url::$api['todo']."{$unionId}/tasks/sources/$sourceId";
-       
+    public static function get_by_sourceId(string $sourceId)
+    {
+        $unionId = UserInfo::$unionId;
+        $uri = Url::$api['todo'] . "{$unionId}/tasks/sources/$sourceId";
+
         return apiRequest::get($uri);
     }
-    public static function query(string $nextToken='',bool $isDone){
-        $unionId=UserInfo::$unionId;
-        $uri=Url::$api['todo']."{$unionId}/org/tasks/query";
-       
-      
-        $body=[
-            'executorStatusList'=>[
-                'nextToken'=>$nextToken,
-                'isDone'=>$isDone
+    public static function query(string $nextToken = '', bool $isDone)
+    {
+        $unionId = UserInfo::$unionId;
+        $uri = Url::$api['todo'] . "{$unionId}/org/tasks/query";
+
+
+        $body = [
+            'executorStatusList' => [
+                'nextToken' => $nextToken,
+                'isDone' => $isDone
             ]
         ];
-        return apiRequest::post($uri,$body);
+        return apiRequest::post($uri, $body);
     }
 }
