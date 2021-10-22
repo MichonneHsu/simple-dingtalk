e7534fd7632459d1e5883e83ca171d83a1c747b6
MichonneHsu <850574565@qq.com>
2021-10-22 15:31:34 +0800

    dd

--- src/v2/Calendar.php
+++ src/v2/Calendar.php
@@ -10,11 +10,129 @@ use SimpleDingTalk\util\date\Time;
 class Calendar
 {
     private static $calendarId = 'primary';
-    public static function create(array $body)
+    public static function create(string $unionId ,array $body)
     {
-        $unionId = UserInfo::$unionId;
+     
 
         $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . '/events';
+        $body=self::date_parse($body);
+        return apiRequest::post($uri, $body);
+    }
+    public static function remove(string $id,string $unionId)
+    {
+       
+
+        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events/{$id}";
+
+
+        return apiRequest::delete($uri);
+    }
+    public static function update(string $id,string $unionId, array $body)
+    {
+      
+
+        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events/{$id}";
+       
+        $body=self::date_parse($body);
+        return apiRequest::put($uri, $body);
+    }
+    public static function get_details(string $id,string $unionId)
+    {
+        
+
+        $uri = Url::$api['calendar'] . "{$unionId}/calendars" . self::$calendarId . "/events/{$id}";
+
+
+        return apiRequest::get($uri);
+    }
+    public static function get_list(string $unionId,array $query)
+    {
+     
+
+        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events";
+        $uri = apiRequest::joinParams($uri, $query);
+
+        return apiRequest::get($uri);
+    }
+    public static function add_attendees(string $id,string $unionId, array $attendeesToAdd)
+    {
+       
+
+        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events/{$id}/attendees";
+        $body = [
+            'attendeesToAdd' => [
+                $attendeesToAdd
+            ]
+
+        ];
+
+        return apiRequest::post($uri, $body);
+    }
+    public static function remove_attendees(string $id,string $unionId, array $attendeesToRemove)
+    {
+       
+
+        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events/{$id}/attendees/batchRemove";
+        $body = [
+            'attendeesToRemove' => [
+                $attendeesToRemove
+            ]
+
+        ];
+
+        return apiRequest::post($uri, $body);
+    }
+    public static function respond(string $id,string $unionId, string $responseStatus)
+    {
+       
+
+        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events/{$id}/respond";
+        $body = [
+            'responseStatus' => $responseStatus
+        ];
+
+        return apiRequest::post($uri, $body);
+    }
+    public static function querySchedule(string $unionId,array $userIds, string $startTime, string $endTime)
+    {
+      
+
+        $uri = Url::$api['calendar'] . "{$unionId}/querySchedule";
+        $time = new Time();
+        $startTime=$time->setDate($startTime)->getDate('c');
+        $endTime=$time->setDate($endTime)->getDate('c');
+        $body = [
+            'userIds' => $userIds,
+            'startTime' => $startTime,
+            'endTime' => $endTime
+        ];
+      
+        return apiRequest::post($uri, $body);
+    }
+    public static function get_signin(string $unionId,string $id, string $maxResults, string $type, string $nextToken = '')
+    {
+      
+
+        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events/$id/signin";
+        $query = [
+            'maxResults' => $maxResults,
+            'type' => $type,
+            'nextToken' => $nextToken
+        ];
+        $uri = apiRequest::joinParams($uri, $query);
+
+        return apiRequest::get($uri);
+    }
+
+    public static function get(string $unionId)
+    {
+        $uri = Url::$api['calendar'] . "{$unionId}/calendars";
+
+        return apiRequest::get($uri);
+    }
+
+
+    public static function date_parse(array $body){
         $time=new Time();
         if(array_key_exists('isAllDay',$body)){
             $isAllDay=$body['isAllDay'];
@@ -36,118 +154,7 @@ class Calendar
             $recurrence_range_endDate=$body['recurrence']['range']['endDate'];
             $body['recurrence']['range']['endDate']=$time->setDate($recurrence_range_endDate)->getDate('c');
         }
-        return apiRequest::post($uri, $body);
-    }
-    public static function remove(string $id)
-    {
-        $unionId = UserInfo::$unionId;
 
-        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events/{$id}";
-
-
-        return apiRequest::delete($uri);
-    }
-    public static function update(string $id, array $body)
-    {
-        $unionId = UserInfo::$unionId;
-
-        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events/{$id}";
-
-
-        return apiRequest::put($uri, $body);
-    }
-    public static function get_details(string $id)
-    {
-        $unionId = UserInfo::$unionId;
-
-        $uri = Url::$api['calendar'] . "{$unionId}/calendars" . self::$calendarId . "/events/{$id}";
-
-
-        return apiRequest::get($uri);
-    }
-    public static function get_list(array $query)
-    {
-        $unionId = UserInfo::$unionId;
-
-        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events";
-        $uri = apiRequest::joinParams($uri, $query);
-
-        return apiRequest::get($uri);
-    }
-    public static function add_attendees(string $id, array $attendeesToAdd)
-    {
-        $unionId = UserInfo::$unionId;
-
-        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events/{$id}/attendees";
-        $body = [
-            'attendeesToAdd' => [
-                $attendeesToAdd
-            ]
-
-        ];
-
-        return apiRequest::post($uri, $body);
-    }
-    public static function remove_attendees(string $id, array $attendeesToRemove)
-    {
-        $unionId = UserInfo::$unionId;
-
-        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events/{$id}/attendees/batchRemove";
-        $body = [
-            'attendeesToRemove' => [
-                $attendeesToRemove
-            ]
-
-        ];
-
-        return apiRequest::post($uri, $body);
-    }
-    public static function respond(string $id, string $responseStatus)
-    {
-        $unionId = UserInfo::$unionId;
-
-        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events/{$id}/respond";
-        $body = [
-            'responseStatus' => $responseStatus
-        ];
-
-        return apiRequest::post($uri, $body);
-    }
-    public static function querySchedule(array $userIds, string $startTime, string $endTime)
-    {
-        $unionId = UserInfo::$unionId;
-
-        $uri = Url::$api['calendar'] . "{$unionId}/querySchedule";
-        $time = new Time();
-        $startTime=$time->setDate($startTime)->getDate('c');
-        $endTime=$time->setDate($endTime)->getDate('c');
-        $body = [
-            'userIds' => $userIds,
-            'startTime' => $startTime,
-            'endTime' => $endTime
-        ];
-      
-        return apiRequest::post($uri, $body);
-    }
-    public static function get_signin(string $id, string $maxResults, string $type, string $nextToken = '')
-    {
-        $unionId = UserInfo::$unionId;
-
-        $uri = Url::$api['calendar'] . "{$unionId}/calendars/" . self::$calendarId . "/events/$id/signin";
-        $query = [
-            'maxResults' => $maxResults,
-            'type' => $type,
-            'nextToken' => $nextToken
-        ];
-        $uri = apiRequest::joinParams($uri, $query);
-
-        return apiRequest::get($uri);
-    }
-
-    public static function get(string $unionId)
-    {
-        $uri = Url::$api['calendar'] . "{$unionId}/calendars";
-
-        return apiRequest::get($uri);
+        return $body;
     }
 }
