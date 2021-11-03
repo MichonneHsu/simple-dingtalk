1ab4cba18356f9f8a7b603b44d2b45f798671079
MichonneHsu <850574565@qq.com>
2021-11-03 15:47:19 +0800

    dd

--- src/v2/Badge.php
+++ src/v2/Badge.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace SimpleDingTalk\v2;
 use SimpleDingTalk\Config;
-use SimpleDingTalk\util\IDgenerator;
+
 class Badge{
    
 
-    public static function create(string $codeIdentity,string $status,string $userCorpRelationType,string $userIdentity,string $gmtExpired,array $availableTimes,array $extInfo,string $codeValue)
+    public static function create(string $requestId,string $codeIdentity,string $status,string $userCorpRelationType,string $userIdentity,string $gmtExpired,array $availableTimes,array $extInfo,string $codeValue='')
     {
-        $IDgenerator=new IDgenerator();
-        $requestId=$IDgenerator->make();
+        
         $uri = Url::$api['badge'] .'/codes/userInstances';
         $body=[
             'requestId'=>$requestId,
@@ -23,7 +22,37 @@ class Badge{
             'gmtExpired'=>$gmtExpired,
             'availableTimes'=>$availableTimes,
             'extInfo'=>json_encode($extInfo),
-         
+            'codeValue'=>$codeValue
+        ];
+    
+        return apiRequest::post($uri, $body);
+    }
+    public static function update(string $codeId,string $codeIdentity,string $status,string $userCorpRelationType,string $userIdentity,string $gmtExpired,array $availableTimes,array $extInfo,string $codeValue='')
+    {
+        
+        $uri = Url::$api['badge'] .'/codes/userInstances';
+        $body=[
+            'codeId'=>$codeId,
+            'corpId'=>Config::$app_info['CORP_ID'],
+            'codeIdentity'=>$codeIdentity,
+            'status'=>$status,
+            'userCorpRelationType'=>$userCorpRelationType,
+            'userIdentity'=>$userIdentity,
+            'gmtExpired'=>$gmtExpired,
+            'availableTimes'=>$availableTimes,
+            'extInfo'=>json_encode($extInfo),
+            'codeValue'=>$codeValue
+        ];
+    
+        return apiRequest::post($uri, $body);
+    }
+    public static function decode(string $payCode,string $requestId)
+    {
+        
+        $uri = Url::$api['badge'] .'/codes/decode';
+        $body=[
+            'payCode'=>$payCode,
+            'requestId'=>$requestId
         ];
     
         return apiRequest::post($uri, $body);
