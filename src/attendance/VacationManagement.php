2e7878a1eb2150e5863442de4c10ca843d81c095
MichonneHsu <850574565@qq.com>
2021-11-06 15:05:08 +0800

    假期管理的创建、更新和批量更新假期余额新增字段值格式转换

--- src/attendance/VacationManagement.php
+++ src/attendance/VacationManagement.php
@@ -11,15 +11,13 @@ class VacationManagement
     public static function create(array $json)
     {
         $uri=Url::$api['attendance']['vacationManagement']['create'];
-      
-       
+        $json=self::parse_data($json);
         return apiRequest::post($uri, $json);
     }
     public static function update(array $json)
     {
         $uri=Url::$api['attendance']['vacationManagement']['update'];
-      
-       
+        $json=self::parse_data($json);
         return apiRequest::post($uri, $json);
     }
     public static function remove(string $op_userid,string $leave_code)
@@ -47,7 +45,14 @@ class VacationManagement
     public static function quota_init(string $op_userid,array $leave_quotas)
     {
         $uri=Url::$api['attendance']['vacationManagement']['quota_init'];
-        
+        if(array_key_exists('quota_num_per_day',$leave_quotas)){
+            $quota_num_per_day=$leave_quotas['quota_num_per_day']*100;
+            $leave_quotas['quota_num_per_day']=$quota_num_per_day;
+        }
+        if(array_key_exists('quota_num_per_hour',$leave_quotas)){
+            $quota_num_per_day=$leave_quotas['quota_num_per_hour']*100;
+            $leave_quotas['quota_num_per_hour']=$quota_num_per_day;
+        }
         $json=[
             'op_userid'=>$op_userid,
             'leave_quotas'=>$leave_quotas
@@ -90,5 +95,18 @@ class VacationManagement
         ];
        
         return apiRequest::post($uri, $json);
+    }
+
+    public static function parse_data(array $json){
+        if(array_key_exists('hours_in_per_day',$json)){
+            $hours_in_per_day=$json['hours_in_per_day']*1000;
+            $json['hours_in_per_day']=$hours_in_per_day;
+        }
+        if(array_key_exists('extras',$json)){
+            $extras=json_encode($json['extras']);
+            $json['extras']=$extras;
+        }
+
+        return $json;
     }
 }
