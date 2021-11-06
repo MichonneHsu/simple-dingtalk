3bab89b848d9a82e2bb3fb9a7dc28b54db022671
MichonneHsu <850574565@qq.com>
2021-11-06 14:07:37 +0800

    公告测试完毕--公告类已注释

--- src/Blackboard.php
+++ src/Blackboard.php
@@ -1,8 +1,17 @@
 <?php declare(strict_types=1);
 namespace SimpleDingTalk;
 
+/**
+ * 公告
+ */
 class Blackboard{
-    
+    /**
+     * 获取公告详情
+     *
+     * @param string $operation_userid
+     * @param string $blackboard_id
+     * @return mixed
+     */
     public static function get(string $operation_userid,string $blackboard_id){
         $uri=Url::$api['blackboard']['get'];
         
@@ -12,6 +21,12 @@ class Blackboard{
         ];
         return apiRequest::post($uri, $json);
     }
+    /**
+     * 创建公告
+     *
+     * @param array $json
+     * @return mixed
+     */
     public static function create(array $json){
         $uri=Url::$api['blackboard']['create'];
         $pre=$json;
@@ -20,6 +35,12 @@ class Blackboard{
         ];
         return apiRequest::post($uri, $json);
     }
+    /**
+     * 更新公告
+     *
+     * @param array $json
+     * @return mixed
+     */
     public static function update(array $json){
         $uri=Url::$api['blackboard']['update'];
         $pre=$json;
@@ -28,6 +49,13 @@ class Blackboard{
         ];
         return apiRequest::post($uri, $json);
     }
+    /**
+     * 删除公告
+     *
+     * @param string $operation_userid
+     * @param string $blackboard_id
+     * @return mixed
+     */
     public static function remove(string $operation_userid,string $blackboard_id){
         $uri=Url::$api['blackboard']['remove'];
         
@@ -37,6 +65,12 @@ class Blackboard{
         ];
         return apiRequest::post($uri, $json);
     }
+    /**
+     * 获取用户可查看的公告
+     *
+     * @param string $userid
+     * @return mixed
+     */
     public static function listtopten(string $userid){
         $uri=Url::$api['blackboard']['listtopten'];
         $json=[
@@ -44,6 +78,12 @@ class Blackboard{
         ];
         return apiRequest::post($uri, $json);
     }
+    /**
+     * 获取公告分类列表
+     *
+     * @param string $operation_userid
+     * @return mixed
+     */
     public static function category_list(string $operation_userid){
         $uri=Url::$api['blackboard']['category_list'];
         $json=[
@@ -51,6 +91,17 @@ class Blackboard{
         ];
         return apiRequest::post($uri, $json);
     }
+    /**
+     * 获取公告ID列表
+     *
+     * @param string $operation_userid
+     * @param string $start_time
+     * @param string $end_time
+     * @param integer $page
+     * @param integer $page_size
+     * @param string $category_id
+     * @return mixed
+     */
     public static function listids(string $operation_userid,string $start_time,string $end_time,int $page,int $page_size=10,string $category_id=''){
         $uri=Url::$api['blackboard']['listids'];
         
