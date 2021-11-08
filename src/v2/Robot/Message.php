855bf46511fd672bcbd5df446ef0357fe0d17dc7
MichonneHsu <850574565@qq.com>
2021-11-08 13:30:53 +0800

    测试机器人

--- src/v2/Robot/Message.php
+++ src/v2/Robot/Message.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace SimpleDingTalk\v2\Robot;
 use SimpleDingTalk\Config;
-use SimpleDingTalk\Url;
+use SimpleDingTalk\v2\Url;
 class Message{
    
     /**
@@ -19,6 +19,7 @@ class Message{
     {
         $robot_type=Config::$robot_type;
         $robotCode=Config::$app_info['robot'][$robot_type]['info']['APP_KEY'];
+      
         $uri = Url::$api['robot']['oToMessages_batchSend'];
         $body=[
             'robotCode'=>$robotCode,
