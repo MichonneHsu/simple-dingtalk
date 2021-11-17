f3bfe3e1526e85dbc782a0aa81950cd983ad7816
MichonneHsu <850574565@qq.com>
2021-11-17 14:10:38 +0800

    更新免登逻辑代码

--- src/v2/AccessToken.php
+++ src/v2/AccessToken.php
@@ -10,8 +10,8 @@ use Exception;
 class AccessToken
 {
 
-    public static $grantType='';
-    public static $code='';
+    public static $grantType = '';
+    public static $code = '';
     public static $refreshToken = '';
 
     public static function getToken(): string
@@ -66,49 +66,58 @@ class AccessToken
     {
         $uri = Url::$api['getUserToken'];;
         $app = Config::$app_info['app'][Config::$app_type];
-        $appkey = $app['app_info']['APP_KEY'];
-        $appSecret = $app['app_info']['APP_SECRET'];
         $body = [
-            'clientId' => $appkey,
-            'clientSecret' => $appSecret,
+            'clientId' => $app['app_info']['APP_KEY'],
+            'clientSecret' => $app['app_info']['APP_SECRET'],
             'grantType' => self::$grantType,
             'code' =>  self::$code,
             'refreshToken' =>  self::$refreshToken
         ];
         $has_token = false;
         $res = apiRequest::post($uri, $body, $has_token);
-
-        $filename = $app['userAccessToken']['file_path'];
         $token = json_decode($res, true);
         $expires_in = $token['expireIn'];
         $token['expireIn'] = $expires_in + time();
-        $data = json_encode($token);
-        file_put_contents($filename, $data);
+
+        return $token;
+      
     }
 
-    public static function getUserToken():string
+    public static function setUserToken(string $unionId)
     {
+        $uri = Url::$api['contact'] . "users/$unionId";
         $at = Config::$app_info['app'][Config::$app_type]['userAccessToken'];
         $file_path = $at['file_path'];
+
+        $data = [];
+
         if (!file_exists($file_path)) {
             throw new Exception($file_path . ' 文件不存在');
         }
-
-        $json = file_get_contents($file_path);
-        if (empty($json)) {
-
-            self::generateUserToken();
+        $file_contents = file_get_contents($file_path);
+        if ($unionId == 'me') {
+            $generatedUserToken = AccessToken::generateUserToken();
+            $accessToken = $generatedUserToken['accessToken'];
+            $res = apiRequest::userGetReq($uri, [], $accessToken);
+            $userinfo = json_decode($res, true);
+            $key = $userinfo['unionId'];
+            if (empty($file_contents) || array_key_exists($key, $userinfo) <> true || json_decode($file_contents, true)[$unionId]['token_info']['expireIn'] - $at['expires'] < time()) {
+                $data[$key] = ['user_info' => $userinfo, 'token_info' => $generatedUserToken];
+            }
+            return file_put_contents($file_path, json_encode($data)) <> false ? true : false;
         } else {
-            $token = json_decode($json, true);
-            if (($token['expireIn'] - $at['expires']) < time()) {
-                self::generateUserToken();
+            $fc_arr = json_decode($file_contents, true);
+            if (empty($file_contents) || array_key_exists($unionId, $fc_arr) <> true || $fc_arr[$unionId]['token_info']['expireIn'] - $at['expires'] < time()) {
+                
+                $generatedUserToken = AccessToken::generateUserToken();
+                $accessToken = $generatedUserToken['accessToken'];
+                $res = apiRequest::userGetReq($uri, [], $accessToken);
+                $userinfo = json_decode($res, true);
+                $data[$unionId] = ['user_info' => $userinfo, 'token_info' => $generatedUserToken];
+
+                return file_put_contents($file_path, json_encode($data)) <> false ? true : false;
             }
         }
-
-        $json = file_get_contents($file_path);
-
-        $token = json_decode($json, true);
-
-        return  $token['accessToken'];
+        return true;
     }
 }
--- src/v2/Contact.php
+++ src/v2/Contact.php
@@ -4,39 +4,42 @@ declare(strict_types=1);
 
 namespace SimpleDingTalk\v2;
 
-
+use SimpleDingTalk\Config;
+use Exception;
 
 class Contact
 {
     public static function getPersonalInfo(string $unionId)
     {
-        $uri = Url::$api['contact']."users/$unionId";
-      
-        return apiRequest::userGetReq($uri);
+        $at = Config::$app_info['app'][Config::$app_type]['userAccessToken'];
+        $file_path = $at['file_path'];
+        if(AccessToken::setUserToken($unionId)){
+            return file_get_contents($file_path);
+        }
+        return '';
     }
-    public static function invites_infos(string $inviterUserId='',string $deptId='')
+    public static function invites_infos(string $inviterUserId = '', string $deptId = '')
     {
-        $uri = Url::$api['contact'].'invites/infos';
-        $params=[
-            'inviterUserId'=>$inviterUserId,
-            'deptId'=>$deptId
+        $uri = Url::$api['contact'] . 'invites/infos';
+        $params = [
+            'inviterUserId' => $inviterUserId,
+            'deptId' => $deptId
         ];
         $uri = apiRequest::joinParams($uri, $params);
         return apiRequest::get($uri);
     }
     public static function dingIndexs()
     {
-        $uri = Url::$api['contact'].'dingIndexs';
-      
+        $uri = Url::$api['contact'] . 'dingIndexs';
+
         return apiRequest::get($uri);
     }
     public static function depts_settings_priorities(bool $enable)
     {
-        $uri = Url::$api['contact'].'depts/settings/priorities';
-        $body=[
-            'enable'=>$enable
+        $uri = Url::$api['contact'] . 'depts/settings/priorities';
+        $body = [
+            'enable' => $enable
         ];
-        return apiRequest::post($uri,$body);
+        return apiRequest::post($uri, $body);
     }
-    
 }
