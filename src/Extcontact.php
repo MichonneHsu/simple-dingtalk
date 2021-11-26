82f511119494d0296b2ef0b620abc395f623655b
MichonneHsu <850574565@qq.com>
2021-11-26 17:23:55 +0800

    Update Extcontact.php

--- src/Extcontact.php
+++ src/Extcontact.php
@@ -1,42 +1,91 @@
 <?php declare(strict_types=1);
 namespace SimpleDingTalk;
-
+/**
+ * 外部联系人
+ */
 class Extcontact{
-
+    /**
+     * 添加外部联系人
+     *
+     * @param array $contact
+     * @return mixed
+     */
     public static function create(array $contact)
     {
         $uri = Url::$api['extcontact']['create'];
         $json=$contact;
         return ApiRequest::post($uri, $json);
     }
-    public static function remove(array $contact)
+    /**
+     * 删除外部联系人
+     *
+     * @param string $user_id
+     * @return mixed
+     */
+    public static function remove(string $user_id)
     {
         $uri = Url::$api['extcontact']['remove'];
-        $json=$contact;
+        $json=[
+            'user_id'=>$user_id
+        ];
         return ApiRequest::post($uri, $json);
     }
+    /**
+     * 更新外部联系人
+     *
+     * @param array $contact
+     * @return mixed
+     */
     public static function update(array $contact)
     {
         $uri = Url::$api['extcontact']['update'];
         $json=$contact;
         return ApiRequest::post($uri, $json);
     }
-    public static function list(array $contact)
+    /**
+     * 获取外部联系人列表
+     *
+     * @param integer $offset
+     * @param integer $size
+     * @return mixed
+     */
+    public static function list(int $offset=0,int $size=20)
     {
         $uri = Url::$api['extcontact']['list'];
-        $json=$contact;
+        $json=[
+            'offset'=>$offset,
+            'size'=>$size
+        ];
         return ApiRequest::post($uri, $json);
     }
-    public static function listlabelgroups(array $contact)
+    /**
+     * 获取外部联系人标签列表
+     *
+     * @param integer $offset
+     * @param integer $size
+     * @return mixed
+     */
+    public static function listlabelgroups(int $offset=0,int $size=20)
     {
         $uri = Url::$api['extcontact']['listlabelgroups'];
-        $json=$contact;
+        $json=[
+            'offset'=>$offset,
+            'size'=>$size
+        ];
         return ApiRequest::post($uri, $json);
     }
-    public static function get(array $contact)
+    /**
+     * 获取外部联系人详情
+     *
+     * @param string $user_id
+     * @return mixed
+     */
+    public static function get(string $user_id)
     {
         $uri = Url::$api['extcontact']['get'];
-        $json=$contact;
+        $json=[
+            'user_id'=>$user_id
+        ];
         return ApiRequest::post($uri, $json);
     }
 
