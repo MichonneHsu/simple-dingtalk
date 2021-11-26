0e545509a646f56d44b770de45a3915e366b744a
MichonneHsu <850574565@qq.com>
2021-11-26 11:53:14 +0800

    Update SceneGroup.php

--- src/chat/SceneGroup.php
+++ src/chat/SceneGroup.php
@@ -9,16 +9,35 @@ use SimpleDingTalk\ApiRequest;
  * 场景群
  */
 class SceneGroup{
+
+    /**
+     * 创建场景群
+     *
+     * @param array $json
+     * @return mixed
+     */
     public static function create(array $json){
         $uri = Url::$api['chat']['scenegroup']['create'];
       
         return ApiRequest::post($uri, $json);
     }
+    /**
+     * 更新场景群
+     *
+     * @param array $json
+     * @return mixed
+     */
     public static function update(array $json){
         $uri = Url::$api['chat']['scenegroup']['update'];
       
         return ApiRequest::post($uri, $json);
     }
+    /**
+     * 查询群信息
+     *
+     * @param string $open_conversation_id
+     * @return mixed
+     */
     public static function get(string $open_conversation_id){
         $uri = Url::$api['chat']['scenegroup']['get'];
         $json=[
@@ -26,6 +45,13 @@ class SceneGroup{
         ];
         return ApiRequest::post($uri, $json);
     }
+    /**
+     * 新增群成员
+     *
+     * @param string $open_conversation_id
+     * @param string $user_ids
+     * @return mixed
+     */
     public static function add_member(string $open_conversation_id,string $user_ids){
         $uri = Url::$api['chat']['scenegroup']['add_member'];
         $json=[
@@ -34,6 +60,13 @@ class SceneGroup{
         ];
         return ApiRequest::post($uri, $json);
     }
+    /**
+     * 删除群成员
+     *
+     * @param string $open_conversation_id
+     * @param string $user_ids
+     * @return mixed
+     */
     public static function remove_member(string $open_conversation_id,string $user_ids){
         $uri = Url::$api['chat']['scenegroup']['remove_member'];
         $json=[
@@ -42,6 +75,14 @@ class SceneGroup{
         ];
         return ApiRequest::post($uri, $json);
     }
+    /**
+     * 查询群成员
+     *
+     * @param string $open_conversation_id
+     * @param string $cursor
+     * @param integer $size
+     * @return mixed
+     */
     public static function get_member(string $open_conversation_id,string $cursor='0',int $size=10){
         $uri = Url::$api['chat']['scenegroup']['get_member'];
         $json=[
