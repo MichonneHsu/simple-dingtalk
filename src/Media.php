73129e66c4b829102150e375b4b30e6b8c1f938e
MichonneHsu <850574565@qq.com>
2021-12-08 10:53:29 +0800

    dd

--- src/Media.php
+++ src/Media.php
@@ -5,14 +5,23 @@ declare(strict_types=1);
 namespace SimpleDingTalk;
 use SimpleDingTalk\DingSpace\ApiRequest;
 use CURLFile;
-
+/**
+ * 文件存储
+ */
 class Media
 {
-    public static function upload(string $file)
+    /**
+     * 上传媒体文件
+     *
+     * @param string $file
+     * @param string $type
+     * @return mixed
+     */
+    public static function upload(string $file,string $type)
     {
         $uri = Url::$api['media']['upload'];
       
-        $file_infos=['media' => new CURLFILE($file),'type'=>'file'];
+        $file_infos=['media' => new CURLFILE($file),'type'=>$type];
         return ApiRequest::upload_file($uri, [], $file_infos);
     }
 }
