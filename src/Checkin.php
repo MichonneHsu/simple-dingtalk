b0f02dfd88b4358046d4788d9da73694213f4d84
MichonneHsu <850574565@qq.com>
2021-12-13 10:13:00 +0800

    dd

--- src/Checkin.php
+++ src/Checkin.php
@@ -4,11 +4,22 @@ declare(strict_types=1);
 
 namespace SimpleDingTalk;
 use SimpleDingTalk\util\Time;
+use SimpleDingTalk\AccessToken;
 /**
  * 签到
  */
 class Checkin
 {
+    /**
+     * 获取用户签到记录
+     *
+     * @param string $userid_list
+     * @param string $start_time
+     * @param string $end_time
+     * @param integer $cursor
+     * @param integer $size
+     * @return mixed
+     */
     public static function get(string $userid_list,string $start_time,string $end_time,int $cursor=0,int $size=10){
         $uri=Url::$api['checkin']['get'];
         $start_time=Time::toTime($start_time,true);
@@ -22,12 +33,17 @@ class Checkin
         ];
         return ApiRequest::post($uri, $json);
     }
-
-    public static function record(array $json){
+    /**
+     * 获取部门用户签到记录
+     *
+     * @param array $query
+     * @return mixed
+     */
+    public static function record(array $query){
         $uri=Url::$api['checkin']['record'];
-        $json['start_time']=Time::toTime($json['start_time'],true);
-        $json['end_time']=Time::toTime($json['end_time'],true);
-        
-        return ApiRequest::post($uri, $json);
+        $query['start_time']=Time::toTime($query['start_time'],true);
+        $query['end_time']=Time::toTime($query['end_time'],true);
+        $query['access_token']=AccessToken::getToken();
+        return ApiRequest::get($uri, $query,false);
     }
 }
