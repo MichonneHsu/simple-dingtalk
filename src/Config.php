b479d4ba984a1d90c566bd78c929f176cb9af016
MichonneHsu <850574565@qq.com>
2022-01-20 14:47:04 +0800

    新增链式配置应用信息方式

--- src/Config.php
+++ src/Config.php
@@ -78,106 +78,7 @@ class Config
                     'expires' => 0,
                     'file_path' => ''
                 ]
-            ],
-
-        // 'CORP_ID' => '',
-        // 'app' => [
-        //     'miniprogram_app' => [
-        //         'app_info' => [
-        //             'AGENT_ID' => '',
-        //             'APP_KEY' => '',
-        //             'APP_SECRET' => ''
-        //         ],
-        //         'access_token' => [
-        //             'expires' => 0,
-        //             'file_path' => ''
-        //         ],
-        //         'callback_info' => [
-        //             'aes_key' => '',
-        //             'token' => ''
-        //         ],
-        //         'login_info' => [
-        //             'autherize' => [
-        //                 'redirect_uri' => ''
-        //             ]
-        //         ],
-        //         'v2' => [
-        //             'access_token' => [
-        //                 'expires' => 0,
-        //                 'file_path' => ''
-        //             ],
-
-        //         ],
-        //         'userAccessToken' => [
-        //             'expires' => 0,
-        //             'file_path' => ''
-        //         ]
-        //     ],
-        //     'micro_app' => [
-        //         'app_info' => [
-        //             'AGENT_ID' => '',
-        //             'APP_KEY' => '',
-        //             'APP_SECRET' => ''
-        //         ],
-        //         'access_token' => [
-        //             'expires' => 0,
-        //             'file_path' => ''
-        //         ],
-        //         'callback_info' => [
-        //             'aes_key' => '',
-        //             'token' => ''
-        //         ],
-        //         'page' => [
-        //             'app' => '',
-        //             'pc' => '',
-        //             'management' => ''
-        //         ],
-        //         'login_info' => [
-        //             'autherize' => [
-        //                 'redirect_uri' => ''
-        //             ]
-        //         ],
-        //         'v2' => [
-        //             'access_token' => [
-        //                 'expires' => 0,
-        //                 'file_path' => ''
-        //             ],
-        //         ],
-        //         'userAccessToken' => [
-        //             'expires' => 0,
-        //             'file_path' => ''
-        //         ]
-        //     ],
-        // ],
-        // 'robot' => [
-        //     'robot1' => [
-        //         'info' => [
-        //             'AGENT_ID' => 0,
-        //             'APP_KEY' => '',
-        //             'APP_SECRET' => '',
-        //             'access_token' => '',
-        //             'SEC' => ''
-        //         ],
-        //         'access_token' => [
-        //             'expires' => 0,
-        //             'file_path' => ''
-        //         ]
-        //     ],
-        //     'robot2' => [
-        //         'info' => [
-        //             'AGENT_ID' => 0,
-        //             'APP_KEY' => '',
-        //             'APP_SECRET' => '',
-        //             'access_token' => '',
-        //             'SEC' => ''
-        //         ],
-        //         'access_token' => [
-        //             'expires' => 0,
-        //             'file_path' => ''
-        //         ]
-        //     ],
-        // ]
-
+            ]
     ];
     public static $robot_info = [
         'robot1' => [
@@ -210,22 +111,27 @@ class Config
     public static function setAppType(string $appName)
     {
         self::$app_type = $appName;
+        return new Self;
     }
     public static function setRobotType(string $robotType)
     {
         self::$robot_type = $robotType;
+        return new Self;
     }
     public static function setCorpId(string $corp_id)
     {
         self::$corp_id = $corp_id;
+        return new Self;
     }
     public static function setApp(array $apps)
     {
         self::$app_info = $apps;
+        return new Self;
     }
     public static function setRobot(array $robots)
     {
         self::$robot_info = $robots;
+        return new Self;
     }
     public static function getApp(){
         return self::$app_info[self::$app_type];
