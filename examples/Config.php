50b05454aac8fdde36f449a22e376946c7e829f5
MichonneHsu <850574565@qq.com>
2022-01-24 16:16:12 +0800

    dd

---
+++ examples/Config.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once './vendor/autoload.php';
+use SimpleDingTalk\Config;
+// 配置信息
+$apps=[
+	'miniprogram_app' => [
+		'info' => [
+			'AGENT_ID' => 123456,
+			'APP_KEY' => '',
+			'APP_SECRET' => '',
+		],
+		'access_token' => [
+			'expires' => 180,
+			'file_path' => './a.json'
+		],
+		'login_info' => [
+			'autherize' => [
+				'redirect_uri' => 'https://www.dingtalk.com',
+				'dingtalk_login_uri'=>'https://www.dingtalk.com'
+			]
+		],
+		'v2' => [
+			'access_token' => [
+				'expires' => 180,
+				'file_path' => './c.json'
+			]
+		],
+		'userAccessToken' => [
+			'expires' => 180,
+			'file_path' => './uat.json'
+		]
+	],
+];
+
+$robots=[
+	'robot1' => [
+		'info' => [
+			'AGENT_ID' => 123456,
+			'APP_KEY' => '',
+			'APP_SECRET' => '',
+			'access_token' => '',
+			'SEC' => ''
+		],
+		'access_token' => [
+			'expires' => 180,
+			'file_path' => './robot.json'
+		]
+
+	],
+];
+Config::setRobot($robots)->
+setApp($apps)->
+setAppType('miniprogram_app')->
+setRobotType('robot1')->
+setCorpId('dasd123465');
