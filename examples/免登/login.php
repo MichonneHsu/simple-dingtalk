39454d32e6580b2e08e2cebe7a9f5c8296c28bc4
MichonneHsu <850574565@qq.com>
2022-01-25 10:11:29 +0800

    新增免登示例

---
+++ examples/免登/login.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once '../Config.php';
+
+use SimpleDingTalk\v2\Authentication\Authorize;
+use SimpleDingTalk\v2\AccessToken;
+use SimpleDingTalk\v2\Contact;
+// 通过Authorize类去获取统一第三方免登链接
+$url=Authorize::assemble_url();
+// 结果：https://login.dingtalk.com/oauth2/auth?client_id=APP_KEY&response_type=code&scope=openid&state=123aawq&prompt=consent&redirect_uri=https%3A%2F%2Fwww.dingtalk.com/login/authPage.html
+// 会跳转在配置里设置好的`·`redirect_uri`参数
+// 钉钉会返回一个`code`的参数
+// 设置一下v2的 获取个授权人信息的Token配置
+$authCode=$_GET['code'];
+AccessToken::setGrantType('authorization_code')->setCode($authCode);
+// 最后Contact类调用getPersonalInfo并设置 me 为参数
+$userinfo=Contact::getPersonalInfo('me');
+// 打印结果出来
+print_r($userinfo);
