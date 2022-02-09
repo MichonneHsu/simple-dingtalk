2f580678ddba42258f1636dce1e9c6f41c964220
MichonneHsu <850574565@qq.com>
2022-02-09 09:12:15 +0800

    更新免登Demo案例

--- examples/免登/login.php
+++ examples/免登/login.php
@@ -9,9 +9,9 @@ use SimpleDingTalk\v2\Contact;
 $url=Authorize::assemble_url();
 // 结果：https://login.dingtalk.com/oauth2/auth?client_id=APP_KEY&response_type=code&scope=openid&state=123aawq&prompt=consent&redirect_uri=https%3A%2F%2Fwww.dingtalk.com/login/authPage.html
 // 会跳转在配置里设置好的`·`redirect_uri`参数
-// 钉钉会返回一个`code`的参数
+// 钉钉会返回一个`authCode`的参数
 // 设置一下userAccessToken的 获取个授权人信息的Token配置
-$authCode=$_GET['code'];
+$authCode=$_GET['authCode'];
 AccessToken::setCode($authCode);
 // 最后Contact类调用getPersonalInfo并设置 me 为参数
 $userinfo=Contact::getPersonalInfo('me');
