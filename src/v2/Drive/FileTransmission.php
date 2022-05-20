e89f49b86e38a368dae9e9efbefd9dcaee01914b
MichonneHsu <850574565@qq.com>
2022-05-20 14:26:07 +0800

    新版钉盘OSS上传信息获取简化参数

--- src/v2/Drive/FileTransmission.php
+++ src/v2/Drive/FileTransmission.php
@@ -3,14 +3,16 @@
 declare(strict_types=1);
 
 namespace SimpleDingTalk\v2\Drive;
+
 use SimpleDingTalk\v2\ApiRequest;
 use SimpleDingTalk\v2\Url;
+
 /**
  * 文件传输
  */
 class FileTransmission
 {
-     /**
+    /**
      * 获取文件下载信息
      *
      * @param string $unionId
@@ -20,14 +22,14 @@ class FileTransmission
      * @param boolean $withInternalResourceUrl
      * @return mixed
      */
-    public static function downloadInfos(string $unionId, string $spaceId,string $fileId,bool $withRegion=false,bool $withInternalResourceUrl=false)
+    public static function downloadInfos(string $unionId, string $spaceId, string $fileId, bool $withRegion = false, bool $withInternalResourceUrl = false)
     {
-        $uri = Url::$api['drive']."/$spaceId/files/$fileId/downloadInfos";
+        $uri = Url::$api['drive'] . "/$spaceId/files/$fileId/downloadInfos";
 
         $params = [
             'unionId' => $unionId,
-            'withRegion'=>$withRegion,
-            'withInternalResourceUrl'=>$withInternalResourceUrl
+            'withRegion' => $withRegion,
+            'withInternalResourceUrl' => $withInternalResourceUrl
         ];
         $uri = ApiRequest::joinParams($uri, $params);
         return ApiRequest::get($uri);
@@ -42,17 +44,18 @@ class FileTransmission
      * @param string $fileSize
      * @return mixed
      */
-    public static function uploadInfos(string $unionId, string $spaceId,string $parentId,string $fileName,string $fileSize,string $md5,string $addConflictPolicy='returnError',string $mediaId='')
+    public static function uploadInfos(string $unionId, string $spaceId, string $parentId, string $fileName, string $addConflictPolicy = 'returnError', string $mediaId = '')
     {
-        $uri = Url::$api['drive']."/$spaceId/files/$parentId/uploadInfos";
-
+        $uri = Url::$api['drive'] . "/$spaceId/files/$parentId/uploadInfos";
+        $fileSize = filesize($fileName);
+        $md5 =md5_file($fileName);
         $params = [
             'unionId' => $unionId,
-            'fileName'=>$fileName,
-            'fileSize'=>$fileSize,
-            'md5'=>$md5,
-            'addConflictPolicy'=>$addConflictPolicy,
-            'mediaId'=>$mediaId
+            'fileName' => $fileName,
+            'fileSize' => $fileSize,
+            'md5' => $md5,
+            'addConflictPolicy' => $addConflictPolicy,
+            'mediaId' => $mediaId
         ];
         $uri = ApiRequest::joinParams($uri, $params);
         return ApiRequest::get($uri);
