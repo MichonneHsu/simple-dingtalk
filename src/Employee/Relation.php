19a5d80813600da90f426c4d1c6b41d25b4d3bbb
MichonneHsu <850574565@qq.com>
2022-06-17 16:21:07 +0800

    智能人事代码调整

---
+++ src/Employee/Relation.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleDingTalk\Employee;
+
+use SimpleDingTalk\Url;
+use SimpleDingTalk\ApiRequest;
+
+/**
+ * 员工管理
+ */
+class Relation
+{
+   
+    /**
+     * 添加企业待入职员工
+     *
+     * @param array $json
+     * @return void
+     */
+    public static function addpreentry(array $json)
+    {
+        $uri = Url::$api['humanResource']['addpreentry'];
+        $pre=$json;
+        $json=[
+            'param'=>$pre
+        ];
+        return ApiRequest::post($uri, $json);
+    }
+}
