7bbee4fb0939d3f3a546f098a39f5ec98f2af640
MichonneHsu <850574565@qq.com>
2023-10-26 17:54:33 +0800

    交互卡片代码更新

--- src/v2/Robot/InteractiveCard.php
+++ src/v2/Robot/InteractiveCard.php
@@ -7,14 +7,13 @@ namespace SimpleDingTalk\v2\Robot;
 use SimpleDingTalk\Config;
 use SimpleDingTalk\v2\Url;
 
-use SimpleDingTalk\util\robot\Sign;
-use SimpleDingTalk\util\Time;
-
 class InteractiveCard
 {
-    private $version='v1';
+    private static $version='v1';
     public static function create(array $body){
         $uri = Url::$api['interactiveCard'][self::$version]['instances'];
+        $cardParamMap=$body['cardData']['cardParamMap'];
+        $body['cardData']['cardParamMap']=json_encode($cardParamMap,JSON_UNESCAPED_UNICODE);
         return ApiRequest::post($uri, $body);
     }
     public static function update(array $body){
@@ -22,6 +21,23 @@ class InteractiveCard
         return ApiRequest::put($uri, $body);
     }
     public static function deliver(array $body){
+        
+        $app=Config::getApp();
+       
+        $robotCode=[
+            'robotCode'=>$app['info']['ROBOT_CODE']
+        ];
+        if(true==array_key_exists('imRobotOpenDeliverModel',$body)){
+            $imRobotOpenDeliverModel=$body['imRobotOpenDeliverModel'];
+            $body['imRobotOpenDeliverModel']=array_merge($imRobotOpenDeliverModel,$robotCode);
+        }
+        if(true==array_key_exists('imGroupOpenDeliverModel',$body)){
+            $imGroupOpenDeliverModel=$body['imGroupOpenDeliverModel'];
+            $body['imGroupOpenDeliverModel']=array_merge($imGroupOpenDeliverModel,$robotCode);
+            if(count($body['imGroupOpenDeliverModel']['extension'])>1){
+                throw new \Exception("extension cannot be more than one", 1);
+            }
+        }
         $uri = Url::$api['interactiveCard'][self::$version]['deliver'];
         return ApiRequest::post($uri, $body);
     }
